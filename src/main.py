019435adfa980cc4a6817b24bfa6ff023f887644
Alexandre Batistella <alexandre.bellas@gmail.com>
2026-05-10 21:13:08 -0300

    feat: add google sheets credentials with ascii handlng

--- src/main.py
+++ src/main.py
@@ -1,14 +1,21 @@
 import json
 
+import requests
+
 from src.client.driver import DriverBuilder
 from src.pages.lme import LmePage
 from src.pages.metal.al import MetalAluminumPage
 from src.pages.metal.ti import MetalTitaniumPage
+from src.settings.credentials import Credentials
+
+# Initialize the credentials
+credentials = Credentials()
 
 # Initialize the driver
 driver = DriverBuilder().driver
 
 try:
+    # Initialize the credentials
     prices: dict[str, str] = {}
 
     # Open the LME page
@@ -36,6 +43,21 @@ try:
     titanium_dioxide_prices = metal_ti_page.get_price("TitaniumDioxide")
     prices = {**prices, **titanium_dioxide_prices}
 
-    print(f"💰 Prices: {json.dumps(prices, indent=4)}")
+    print(f"💰 Prices: {json.dumps(prices, indent=4, ensure_ascii=False)}")
+
+    # Save the prices to the Google Sheets
+    response = requests.post(
+        credentials.google_sheets.endpoint,
+        data=json.dumps(
+            {"prices": prices, "token": credentials.google_sheets.token},
+            ensure_ascii=False,
+        ).encode("utf-8"),
+        headers={"Content-Type": "application/json; charset=utf-8"},
+        timeout=30,
+    )
+    response.raise_for_status()
+
+    print("🐶🐶🐶 Prices gathering completed successfully. Check your Google Sheets for the latest prices.")
+
 except Exception as e:
     print(f"❌ Error: {e}, {e.__traceback__}")
